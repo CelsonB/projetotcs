f2af2f3dceb63c7dec32f8ebba8eadd26ecbf660
Celson Andrade <celson.bac@gmail.com>
2025-02-17 17:27:14 -0300

    Fix: adicionei o banco de dados

--- src/main/java/com/projetotcs/projetotcs/model/Aviso.java
+++ src/main/java/com/projetotcs/projetotcs/model/Aviso.java
@@ -1,31 +1,44 @@
 package com.projetotcs.projetotcs.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "avisos")
 public class Aviso {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private int idCategoria;
+
     private String descricao;
-    
-    
-    
+
+    public Aviso() {
+    }
+
     public Aviso(int idCategoria, String descricao) {
         this.idCategoria = idCategoria;
         this.descricao = descricao;
     }
-    
+
+    public Long getId() {
+        return id;
+    }
+
     public int getIdCategoria() {
         return idCategoria;
     }
+
     public void setIdCategoria(int idCategoria) {
         this.idCategoria = idCategoria;
     }
+
     public String getDescricao() {
         return descricao;
     }
+
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
-
-
-    
-
 }
